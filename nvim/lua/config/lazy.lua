766532b3e33dd97da3487ebf579fa3d93a6deda7
hoangharry-tm <tonminhhoang.work@gmail.com>
2024-09-14 23:42:19 -0400

    minor fix

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -33,6 +33,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "lazyvim.plugins.extras.coding.codeium" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "plugins" },
   },
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-doc-name
 return {
   -- tools
   {
